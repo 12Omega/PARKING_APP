52bd2029f97652c2dfeb920bde58bfbd1b905e6c
Pratham Shrestha <pratham199911@gmail.com>
2025-06-17 21:34:48 +0545

    Build the Login Screen UI

---
+++ lib/features/auth/presentation/widgets/app_text_field.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../../../../constants/colors.dart';
+
+class AppTextField extends StatelessWidget {
+  final String hintText;
+  final bool obscureText;
+
+  const AppTextField({
+    super.key,
+    required this.hintText,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        filled: true,
+        fillColor: AppColors.inputBackground,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
